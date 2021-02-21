6812c668e061c5044ccaa5032c8c9c9bb366a985
IanTheG <goodwinij1@gmail.com>
2021-02-20 21:36:46 -0600

    Fixing styling on DesktopView component. Added dark/light mode transition, removed enlarging animation for MysteriesNav

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Home from './components/Home'
 import Guide from './components/Guide'
 import OpeningPrayers from './components/OpeningPrayers'
 import MysteriesNav from './components/MysteriesNav'
-import ClosingPrayers from './components/ClosingPrayers'
-import Mystery from './components/Mystery'
 import About from './components/About'
+import Mystery from './components/Mystery'
+import ClosingPrayers from './components/ClosingPrayers'
 
 const App = () => {
 
@@ -18,6 +18,21 @@ const App = () => {
   window.addEventListener('load', setDocHeight)
   window.addEventListener('resize', setDocHeight)
   window.addEventListener('orientationchange', setDocHeight)
+  
+  // // Logic to hide / display views
+  // const toggleViews = () => {
+  //   const deviceWidth = window.screen.orientation
+
+  //   if (deviceWidth) {
+
+  //   }
+  // }
+  // window.addEventListener('resize', toggleViews)
+
+  // if window is in portrait mode and less than 1024px, display app
+  // if window is in landscape mode and on mobile, display rotate to portrait and hide desktop
+  // if window is in landscape mode and on desktop, display desktop view
+      // if window is less than 1024px, hide desktop view
 
   return (
     <main id="root" className="scroll-container">
